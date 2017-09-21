79929593bbe1bdd2214090fdd7a42853accf921b
海老原祐輔 <ebiyuu1121@gmail.com>
2017-09-21 23:50:34 +0900

    [add] leapfrog-makelist.py leapfrog-return.py

---
+++ leapfrog-makelist.py
@@ -0,0 +1,53 @@
+#coding=utf-8
+import math
+
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+
+from leapfrogreturn import leapfrog
+
+import sys
+args=sys.argv
+if(len(args)!=11):
+    print('Error!')
+    exit()
+
+v0=float(args[1])
+thetadeg=float(args[2])
+g=float(args[3])
+m=float(args[4])
+k=float(args[5])
+rate=float(args[6])
+
+vartype=args[7]
+minval=float(args[8])
+maxval=float(args[9])
+rate2=int(args[10])
+
+
+df=pd.DataFrame(columns=['v0','thetadeg','g','m','k','rate','t','x','y','vx','vy','ax','ay'])
+
+for i in range(rate2):
+    if vartype=='v0':
+        v0=minval+(maxval-minval)/rate2*i
+    elif vartype=='thetadeg':
+        thetadeg=minval+(maxval-minval)/rate2*i
+    elif vartype=='g':
+        g=minval+(maxval-minval)/rate2*i
+    elif vartype=='m':
+        m=minval+(maxval-minval)/rate2*i
+    elif vartype=='k':
+        k=minval+(maxval-minval)/rate2*i
+    elif vartype=='rate':
+        rate=minval+(maxval-minval)/rate2*i
+    else:
+        print('Eroor!')
+        exit()
+    
+    ret=leapfrog(v0,thetadeg,g,m,k,rate)
+    
+    df2=pd.DataFrame([[v0,thetadeg,g,m,k,rate,ret['t'],ret['x'],ret['y'],ret['vx'],ret['vy'],ret['ax'],ret['ay']]],columns=['v0','thetadeg','g','m','k','rate','t','x','y','vx','vy','ax','ay'])
+    df=df.append(df2)
+    
+df.to_csv('list/'+vartype+'('+str(minval)+'to'+str(maxval)+'by'+str(rate2)+')'+str(v0)+'-'+str(thetadeg)+'-'+str(g)+'-'+str(m)+'-'+str(k)+'-'+str(rate)+'.csv',index=False)
---
+++ leapfrogreturn.py
@@ -0,0 +1,44 @@
+#coding=utf-8
+def leapfrog(v0,thetadeg,g,m,k,rate):
+    import math
+
+    import pandas as pd
+    import matplotlib
+    import matplotlib.pyplot as plt
+
+    theta=thetadeg/360*2*3.14
+
+    t=0
+    x=0
+    y=0
+
+    vx=v0*math.cos(theta)
+    vy=v0*math.sin(theta)
+    ax=0
+    ay=0
+
+    df=pd.DataFrame(columns=['t','x','y','vx','vy','ax','ay'])
+
+    while(y>=0):
+        vx+=ax/rate
+        vy+=ay/rate
+        
+        ax=(-k*vx)/m
+        ay=(-m*g-k*vy)/m
+        
+        x+=vx/rate
+        y+=vy/rate
+        
+        t+=1/rate
+        
+        df2=pd.DataFrame([[t,x,y,vx,vy,ax,ay]],columns=['t','x','y','vx','vy','ax','ay'])
+        df=df.append(df2)
+        
+    df.to_csv('result/'+str(v0)+'-'+str(thetadeg)+'-'+str(g)+'-'+str(m)+'-'+str(k)+'-'+str(rate)+'.csv',index=False)
+    df.plot.scatter(x='x',y='y')
+    plt.axes().set_aspect('equal',adjustable='box')
+    plt.xlim(xmin=0)
+    plt.ylim(ymin=0)
+    plt.savefig('images/'+str(v0)+'-'+str(thetadeg)+'-'+str(g)+'-'+str(m)+'-'+str(k)+'-'+str(rate)+'.png')
+
+    return({'t':t,'x':x,'y':y,'vx':vx,'vy':vy,'ax':ax,'ay':ay})
