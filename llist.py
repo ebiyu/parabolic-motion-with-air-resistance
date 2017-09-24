213c966195e21196e5ac3c8fd3bbca952975ac7b
海老原祐輔 <ebiyuu1121@gmail.com>
2017-09-24 20:28:48 +0900

    [fix] order of column of dataframe

--- llist.py
+++ llist.py
@@ -55,7 +55,7 @@ for i in range(rate2):
     
     ret=leapfrog(v0,thetadeg,g,m,k,rate,formula,foldername)
     
-    df2=pd.DataFrame([[v0,thetadeg,g,m,k,formula,rate,ret['t'],ret['x'],ret['y'],ret['vx'],ret['vy'],ret['ax'],ret['ay']]],columns=['v0','thetadeg','g','m','k','formula','rate','t','x','y','vx','vy','ax','ay'])
+    df2=pd.DataFrame([[v0,thetadeg,g,m,k,rate,formula,ret['t'],ret['x'],ret['y'],ret['vx'],ret['vy'],ret['ax'],ret['ay']]],columns=['v0','thetadeg','g','m','k','rate','formula','t','x','y','vx','vy','ax','ay'])
     df=df.append(df2)
     
 df.to_csv(foldername+'/list.csv',index=False)
